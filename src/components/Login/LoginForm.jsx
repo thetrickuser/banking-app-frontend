469536fc1f27a630283166e75a7fb637743ce3b8
thetrickuser <coolady1807@outlook.com>
2023-08-07 22:13:11 +0530

    login and register functionality added

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "../UI/Modal/Modal";
 import styles from "./LoginForm.module.css";
 import Input from "../UI/Input/Input";
 import Button from "../UI/Button/Button";
 
 const LoginForm = (props) => {
-  const handleLogin = () => {
-    console.log("Login");
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    const formData = {
+      username,
+      password
+    }
+
+    
+    const response = await fetch('http://localhost:8090/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
   };
   return (
     <Modal onBackdropClick={props.onClose}>
       <div className={styles["login-form"]}>
         <h2>Login Form</h2>
         <form>
-          <Input type="email" label="Email:" />
-          <Input type="password" label="Password:" />
+          <Input type="text" label="Username:" onChange={handleUsernameChange} value={username}/>
+          <Input type="password" label="Password:" onChange={handlePasswordChange} value={password}/>
           <Button
             styles={styles.button}
             type="button"
